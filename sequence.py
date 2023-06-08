732673f171152db038a69a5026af950ecc4b6743
Khaliff <syarif.daus@gmail.com>
2023-06-08 20:37:25 +0700

    learn_sequential

--- sequence.py
+++ sequence.py
@@ -8,7 +8,7 @@ Semua sintaksis dasar bahasa pemrograman terdiri dari:
 # Sequential
 print('Ibu berkata,"Pergi ke toko"')
 print('Budi menjawab, "Baik, apa yang harus dibeli"')
-print('Ibu menjawab, "Beli satu botol susu dan jika ada telur beli 6"')
+print('Ibu menjawab, "Beli satu botol susu dan beli 6 jika ada telur"')
 print("Maka Budi berangkat ke toko")
 print("Dan mulai berbelanja")
 
@@ -18,6 +18,24 @@ jumlah_botol_susu = 6
 if jumlah_botol_susu > 0:
     print("Budi mengecek harganya, dan ternyata uangnya cukup")
     print("Budi membeli 1 botol susu")
+else:
+    print("Budi tidak jadi membeli 1 botol susu")
+
+print("Budi pulang ke rumah")
+print("Menyerahkan hasilnya kepada ibu")
+
+# percabangan2
+jumlah_botol_susu = 60
+jumlah_telur = 100
+print(f"Jumlah Botol Susu {jumlah_botol_susu} botol")
+print(f"Jumlah Telur {jumlah_telur} butir")
+
+if jumlah_botol_susu > 0:
+    print("Budi mengecek harganya, dan ternyata uangnya cukup")
+    if jumlah_telur == 0:
+        print("Budi membeli 1 botol susu")
+    else:
+        print("Budi membeli 1 botol susu & 6 butir telur")
 else:
     print("Budi tidak jadi membeli 1 botol susu")
 
